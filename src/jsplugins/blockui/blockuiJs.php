b3887cb70b0879592576fe1ac14cd6d6f0758326
tigredonorte <tigredonorte3@gmail.com>
2014-10-10 16:02:49 -0300

    duplicated style tags removed

--- src/jsplugins/blockui/blockuiJs.php
+++ src/jsplugins/blockui/blockuiJs.php
@@ -13,7 +13,7 @@ class blockuiJs extends JsPlugin{
     } 
     
     public function init(){
-        static $css = '<style>.closemsg{color:white; font-size:12px; float:right; cursor: Pointer;}</style>';
+        static $css = '.closemsg{color:white; font-size:12px; float:right; cursor: Pointer;}';
         $this->Html->LoadBowerComponent("blockUI/jquery.blockUI");
         $this->Html->LoadBowerComponent('hatframework-hatjs-form/jqueryui/blockui');
         $this->Html->LoadCss("plugins/jqueryui/blockui");
